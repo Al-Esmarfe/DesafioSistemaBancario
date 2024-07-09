a2f1f93d9235d20071e27670c1374d684029db08
Mateus Alves <mateus.paulo.alves@gmail.com>
2024-07-08 22:53:48 -0300

    Desafio Feito

---
+++ Cliente.py
@@ -0,0 +1,22 @@
+class Cliente:
+    def __init__(self, endereco):
+        self.endereco = endereco
+        self._contas = []
+
+    @property
+    def contas(self):
+        return self._contas
+
+    def realizar_transacao(self, conta, transacao):
+        transacao.registrar(conta)
+
+    def adicionar_conta(self, conta):
+        self._contas.append(conta)
+
+
+class PessoaFisica(Cliente):
+    def __init__(self, nome, data_nascimento, cpf, endereco):
+        super().__init__(endereco)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
